18fd6b7be7de1c40ae80773577d245d8f3a0bc6b
Nikolay Noskov <hhb@bk.ru>
2018-09-02 00:02:15 +0300

    Удалил импорт класса Toast

--- app/src/main/java/com/home/hhb/PLCManager/ToolsActivity.java
+++ app/src/main/java/com/home/hhb/PLCManager/ToolsActivity.java
@@ -11,7 +11,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import static com.home.hhb.PLCManager.LaptopServer.LOG_TAG;
 
